2aea0c7bc278c79266c2ae224f92c05ea5453518
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2026-01-09 17:19:29 +0530

    check for deployment

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 
 
-app.listen(3000,()=>{
-    console.log("Server is running port 3000");
-});
+app.get("/health",(req,res)=>{
+    res.status(201).json({ message:"App health is ok"});
+})
